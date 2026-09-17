df1e16b99665dc3d926579e24f1b4dc099f4b0ed
GiaTuong <nguyenhuugiatuong0@gmail.com>
2026-09-17 22:48:03 +0700

    Continue Flask swimclub webapp, add swimmers list, and test __name__ with whoami.py

--- 04-head-first-python-swimclub/webapp/app.py
+++ 04-head-first-python-swimclub/webapp/app.py
@@ -1,9 +1,23 @@
+import os
+import swimclub
 from flask import Flask
 app = Flask(__name__)
 
 @app.get("/")
 def index():
     return "This is a placeholder for your webapp's opening page."
+
+@app.get("/swimmers")
+def display_swimmers():
+    swim_files = os.listdir(swimclub.FOLDER)
+    swim_files.remove(".DS_Store")
+    swimmers = {}
+    for file in swim_files:
+        name, *_=swimclub.read_swim_data(file)
+        if name not in swimmers:
+            swimmers[name] = []
+        swimmers[name].append(file)
+    return str(sorted(swimmers))
 
 if __name__ == "__main__":
     app.run()
