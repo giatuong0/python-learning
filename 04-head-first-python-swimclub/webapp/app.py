fb74589fba69920b6d2a9cf50a300567aba06bfd
GiaTuong <nguyenhuugiatuong0@gmail.com>
2026-09-20 22:06:49 +0700

    Flask session code update

--- 04-head-first-python-swimclub/webapp/app.py
+++ 04-head-first-python-swimclub/webapp/app.py
@@ -1,7 +1,9 @@
 import os
 import swimclub
-from flask import Flask
+from flask import Flask,session 
+
 app = Flask(__name__)
+app.secret_key = "YouWillNeverGuessThisSecretKey"
 
 @app.get("/")
 def index():
@@ -11,13 +13,17 @@ def index():
 def display_swimmers():
     swim_files = os.listdir(swimclub.FOLDER)
     swim_files.remove(".DS_Store")
-    swimmers = {}
+    session["swimmers"] = {}
     for file in swim_files:
         name, *_=swimclub.read_swim_data(file)
-        if name not in swimmers:
-            swimmers[name] = []
-        swimmers[name].append(file)
-    return str(sorted(swimmers))
+        if name not in session["swimmers"]:
+            session["swimmers"][name] = []
+        session["swimmers"][name].append(file)
+    return str(sorted(session["swimmers"]))
+
+@app.get("/files/<swimmer>")
+def get_swimmers_files(swimmer):
+    return str(session["swimmers"][swimmer])
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
